b56019974355bea5ba85f75031d0b83992185cd5
jesuanp <jespaterm@gmail.com>
2022-06-13 11:33:58 -0500

    cambiando la coneccion del socket.io

--- public/modoCompetitivo.js
+++ public/modoCompetitivo.js
@@ -1,218 +1,218 @@
-// const socket = io("http://puzzle-jesuanp.vercel.app/");
+var socket = io.connect("http://puzzle-jesuanp.vercel.app/", { forceNew: true });
 
-// let body = document.body;
+let body = document.body;
 
-// socket.on('ganador', data => { 
+socket.on('ganador', data => { 
     
-//     let ventanaEmergente = document.createElement('div');
-//     ventanaEmergente.className = 'ventana-emergente';
+    let ventanaEmergente = document.createElement('div');
+    ventanaEmergente.className = 'ventana-emergente';
 
-//     let cuerpoVentana = document.createElement('div');
-//     cuerpoVentana.className = 'cuerpo-ventana';
+    let cuerpoVentana = document.createElement('div');
+    cuerpoVentana.className = 'cuerpo-ventana';
 
-//     cuerpoVentana.innerText = 'Ya hay un ganador';
-//     cuerpoVentana.style.color = '#fff';
+    cuerpoVentana.innerText = 'Ya hay un ganador';
+    cuerpoVentana.style.color = '#fff';
 
-//     ventanaEmergente.append(cuerpoVentana)
+    ventanaEmergente.append(cuerpoVentana)
 
-//     body.append(ventanaEmergente);
-// });
+    body.append(ventanaEmergente);
+});
 
-// socket.on('empezar', data => {
+socket.on('empezar', data => {
 
-//     console.log('empezar');
+    console.log('empezar');
 
-//     if(data.socketId !== socket.id){
-//         console.log('generar cartas');
+    if(data.socketId !== socket.id){
+        console.log('generar cartas');
                 
-//         if(data.reload.numCartas === 8){
-//             container.style.gridTemplateColumns = 'repeat(4, 1fr)';
-//         }
-//         if(data.reload.numCartas === 12){
-//             container.style.gridTemplateColumns = 'repeat(6, 1fr)';
-//         }
+        if(data.reload.numCartas === 8){
+            container.style.gridTemplateColumns = 'repeat(4, 1fr)';
+        }
+        if(data.reload.numCartas === 12){
+            container.style.gridTemplateColumns = 'repeat(6, 1fr)';
+        }
 
-//         SeleccionarCartasRandom(data.reload.array, data.reload.numCartas);
-//     }
-//     ponerTablero();
+        SeleccionarCartasRandom(data.reload.array, data.reload.numCartas);
+    }
+    ponerTablero();
 
-// });
+});
 
 
-// let codigo = Math.ceil(Math.random() * (9999 - 1000) + 1000);
-// let sala = document.createElement('div');
-// sala.innerText = 'Sala: ' + codigo;
-// sala.id = 'codigo-sala';
+let codigo = Math.ceil(Math.random() * (9999 - 1000) + 1000);
+let sala = document.createElement('div');
+sala.innerText = 'Sala: ' + codigo;
+sala.id = 'codigo-sala';
 
-// const ponerTablero = () => {
+const ponerTablero = () => {
     
-//     inicio.remove();
-//     body.append(container);
-//     body.append(sala);
+    inicio.remove();
+    body.append(container);
+    body.append(sala);
 
-//     let cuerpoVentana = document.getElementsByClassName('cuerpo-ventana');
-//     cuerpoVentana[0].innerHTML = '';
+    let cuerpoVentana = document.getElementsByClassName('cuerpo-ventana');
+    cuerpoVentana[0].innerHTML = '';
     
-//     let ventanaEmergente = document.getElementsByClassName('ventana-emergente');
-//     ventanaEmergente[0].innerHTML = '';
+    let ventanaEmergente = document.getElementsByClassName('ventana-emergente');
+    ventanaEmergente[0].innerHTML = '';
 
-//     let segundos = document.createElement('div');
-//     segundos.id = 'segundos';
+    let segundos = document.createElement('div');
+    segundos.id = 'segundos';
 
-//     ventanaEmergente[0].append(segundos);
+    ventanaEmergente[0].append(segundos);
 
-//     let cont = 3;
+    let cont = 3;
 
-//     segundos.innerText = cont;
+    segundos.innerText = cont;
 
-//     let cuentaRegresiva = setInterval(() => {
-//         if(cont === 1){
-//             clearInterval(cuentaRegresiva);
-//             ventanaEmergente[0].remove();
-//         }
-//         else {
-//             cont--;
-//             segundos.innerText = cont;
-//         }
-//     }, 1000)
-// }
+    let cuentaRegresiva = setInterval(() => {
+        if(cont === 1){
+            clearInterval(cuentaRegresiva);
+            ventanaEmergente[0].remove();
+        }
+        else {
+            cont--;
+            segundos.innerText = cont;
+        }
+    }, 1000)
+}
 
-// const crearVentanaEmergente = () => {
+const crearVentanaEmergente = () => {
 
-//     let btnVentanaCrear = document.createElement('button');
-//     btnVentanaCrear.className = 'btn-ventana';
-//     btnVentanaCrear.innerText = 'Crear partida';
+    let btnVentanaCrear = document.createElement('button');
+    btnVentanaCrear.className = 'btn-ventana';
+    btnVentanaCrear.innerText = 'Crear partida';
     
-//     let btnVentanaUnirse = document.createElement('button');
-//     btnVentanaUnirse.className = 'btn-ventana';
-//     btnVentanaUnirse.innerText = 'Unirse';
+    let btnVentanaUnirse = document.createElement('button');
+    btnVentanaUnirse.className = 'btn-ventana';
+    btnVentanaUnirse.innerText = 'Unirse';
     
-//     let cuerpoVentana = document.createElement('div');
-//     cuerpoVentana.className = 'cuerpo-ventana';
-//     cuerpoVentana.append(btnVentanaCrear, btnVentanaUnirse);
+    let cuerpoVentana = document.createElement('div');
+    cuerpoVentana.className = 'cuerpo-ventana';
+    cuerpoVentana.append(btnVentanaCrear, btnVentanaUnirse);
     
-//     let ventanaEmergente = document.createElement('div');
-//     ventanaEmergente.className = 'ventana-emergente';
-//     ventanaEmergente.append(cuerpoVentana)
+    let ventanaEmergente = document.createElement('div');
+    ventanaEmergente.className = 'ventana-emergente';
+    ventanaEmergente.append(cuerpoVentana)
     
-//     body.append(ventanaEmergente);
+    body.append(ventanaEmergente);
 
-//     btnVentanaUnirse.addEventListener('click', () => {
+    btnVentanaUnirse.addEventListener('click', () => {
 
-//         ponerCodigo();
-//     })
+        ponerCodigo();
+    })
     
-//     btnVentanaCrear.addEventListener('click', () => {
+    btnVentanaCrear.addEventListener('click', () => {
     
-//         let btnNivelFacil = document.createElement('button')
-//         btnNivelFacil.className = 'btn';
-//         btnNivelFacil.innerText = 'Facíl';
+        let btnNivelFacil = document.createElement('button')
+        btnNivelFacil.className = 'btn';
+        btnNivelFacil.innerText = 'Facíl';
     
-//         let btnNivelIntermedio = document.createElement('button')
-//         btnNivelIntermedio.className = 'btn';
-//         btnNivelIntermedio.innerText = 'Intermedio';
+        let btnNivelIntermedio = document.createElement('button')
+        btnNivelIntermedio.className = 'btn';
+        btnNivelIntermedio.innerText = 'Intermedio';
     
-//         let cuerpoVentana = document.getElementsByClassName('cuerpo-ventana');
-//         cuerpoVentana = cuerpoVentana[0];
+        let cuerpoVentana = document.getElementsByClassName('cuerpo-ventana');
+        cuerpoVentana = cuerpoVentana[0];
     
-//         cuerpoVentana.innerText = '';
-//         cuerpoVentana.append(btnNivelFacil, btnNivelIntermedio);
+        cuerpoVentana.innerText = '';
+        cuerpoVentana.append(btnNivelFacil, btnNivelIntermedio);
     
-//         let btnEmpezar = document.createElement('button');
-//         btnEmpezar.className = 'btn';
-//         btnEmpezar.innerText = 'Empezar';
+        let btnEmpezar = document.createElement('button');
+        btnEmpezar.className = 'btn';
+        btnEmpezar.innerText = 'Empezar';
 
-//         btnNivelFacil.addEventListener('click', () => {
+        btnNivelFacil.addEventListener('click', () => {
 
-//             reload = {array: arrayImagesFacil, numCartas: 8};
+            reload = {array: arrayImagesFacil, numCartas: 8};
 
-//             inicio.remove();
-//             ventanaEmergente.innerText = '';
-//             cuerpoVentana.innerText = '';
-//             cuerpoVentana.append(btnEmpezar);
-//             ventanaEmergente.append(cuerpoVentana);
+            inicio.remove();
+            ventanaEmergente.innerText = '';
+            cuerpoVentana.innerText = '';
+            cuerpoVentana.append(btnEmpezar);
+            ventanaEmergente.append(cuerpoVentana);
     
-//             container.style.gridTemplateColumns = 'repeat(4, 1fr)';
+            container.style.gridTemplateColumns = 'repeat(4, 1fr)';
 
-//             socket.emit('unir-sala', {id: socket.id, codigo});
+            socket.emit('unir-sala', {id: socket.id, codigo});
     
-//             setTimeout(() => {
-//                 body.append(container);
-//                 body.append(sala);
-//                 SeleccionarCartasRandom(arrayImagesFacil, 8);
-//             }, 100);
+            setTimeout(() => {
+                body.append(container);
+                body.append(sala);
+                SeleccionarCartasRandom(arrayImagesFacil, 8);
+            }, 100);
     
-//         });
+        });
     
-//         btnNivelIntermedio.addEventListener('click', () => {
+        btnNivelIntermedio.addEventListener('click', () => {
 
-//             reload = {array: arrayImagesNormal, numCartas: 12};
+            reload = {array: arrayImagesNormal, numCartas: 12};
     
-//             inicio.remove();
-//             ventanaEmergente.innerText = '';
-//             cuerpoVentana.innerText = '';
-//             cuerpoVentana.append(btnEmpezar);
-//             ventanaEmergente.append(cuerpoVentana);
+            inicio.remove();
+            ventanaEmergente.innerText = '';
+            cuerpoVentana.innerText = '';
+            cuerpoVentana.append(btnEmpezar);
+            ventanaEmergente.append(cuerpoVentana);
     
     
-//             if(screen.width > 700) container.style.gridTemplateColumns = 'repeat(6, 1fr)';
-//             else container.style.gridTemplateColumns = 'repeat(4, 1fr)';
+            if(screen.width > 700) container.style.gridTemplateColumns = 'repeat(6, 1fr)';
+            else container.style.gridTemplateColumns = 'repeat(4, 1fr)';
     
-//             socket.emit('unir-sala', {id: socket.id, codigo});
+            socket.emit('unir-sala', {id: socket.id, codigo});
 
-//             setTimeout(() => {
-//                 body.append(container);
-//                 body.append(sala);
-//                 SeleccionarCartasRandom(arrayImagesNormal, 12);
-//             }, 100);
+            setTimeout(() => {
+                body.append(container);
+                body.append(sala);
+                SeleccionarCartasRandom(arrayImagesNormal, 12);
+            }, 100);
     
-//         });
+        });
 
-//         btnEmpezar.addEventListener('click', () => {
+        btnEmpezar.addEventListener('click', () => {
 
-//             fetch('https://puzzle-jesuanp.vercel.app/empezar?sala=' + codigo, {
-//                 method: 'POST',
-//                 body: JSON.stringify({reload, socketId: socket.id}),
-//                 headers:{
-//                     'Content-Type': 'application/json'
-//                 }
-//             });
-//         });
-//     });
-// }
+            fetch('https://puzzle-jesuanp.vercel.app/empezar?sala=' + codigo, {
+                method: 'POST',
+                body: JSON.stringify({reload, socketId: socket.id}),
+                headers:{
+                    'Content-Type': 'application/json'
+                }
+            });
+        });
+    });
+}
 
-// const ponerCodigo = () => {
+const ponerCodigo = () => {
 
-//     let inputVentana = document.createElement('input');
-//     inputVentana.className = 'input-ventana';
-//     inputVentana.placeholder = 'Escribe el código aquí...';
-//     inputVentana.type = 'text';
+    let inputVentana = document.createElement('input');
+    inputVentana.className = 'input-ventana';
+    inputVentana.placeholder = 'Escribe el código aquí...';
+    inputVentana.type = 'text';
 
-//     let btnEnviarVentana = document.createElement('button');
-//     btnEnviarVentana.className = 'btn-enviar-ventana';
-//     btnEnviarVentana.innerText = 'Enviar';
+    let btnEnviarVentana = document.createElement('button');
+    btnEnviarVentana.className = 'btn-enviar-ventana';
+    btnEnviarVentana.innerText = 'Enviar';
 
-//     let cuerpoVentana = document.getElementsByClassName('cuerpo-ventana');
-//     cuerpoVentana = cuerpoVentana[0];
+    let cuerpoVentana = document.getElementsByClassName('cuerpo-ventana');
+    cuerpoVentana = cuerpoVentana[0];
 
-//     cuerpoVentana.innerText = '';
-//     cuerpoVentana.append(inputVentana, btnEnviarVentana);
+    cuerpoVentana.innerText = '';
+    cuerpoVentana.append(inputVentana, btnEnviarVentana);
 
-//     btnEnviarVentana.addEventListener('click', () => {
+    btnEnviarVentana.addEventListener('click', () => {
 
-//         sala.innerText = 'Sala: ' + inputVentana.value;
-//         socket.emit('unir-sala', {id: socket.id, codigo: inputVentana.value});
+        sala.innerText = 'Sala: ' + inputVentana.value;
+        socket.emit('unir-sala', {id: socket.id, codigo: inputVentana.value});
 
-//         cuerpoVentana.innerText = 'Esperando al otro jugador...';
-//         cuerpoVentana.style.color = '#fff'
+        cuerpoVentana.innerText = 'Esperando al otro jugador...';
+        cuerpoVentana.style.color = '#fff'
 
-//     });
-// }
+    });
+}
 
-// function playerWin(sala){
+function playerWin(sala){
 
-//     fetch('http://puzzle-jesuanp.vercel.app/ganador?sala=' + sala, {
-//         method: 'post',
-//     });
-// }
+    fetch('http://puzzle-jesuanp.vercel.app/ganador?sala=' + sala, {
+        method: 'post',
+    });
+}
 
--- public/selectDificulty.js
+++ public/selectDificulty.js
@@ -1,47 +1,47 @@
-// let btns = document.getElementsByClassName('btn');
+let btns = document.getElementsByClassName('btn');
 
-// for(let i = 0; i < btns.length; i++){
+for(let i = 0; i < btns.length; i++){
     
-//     let dificultad = btns[i].textContent;
+    let dificultad = btns[i].textContent;
     
-//     btns[i].addEventListener('click', () => {
+    btns[i].addEventListener('click', () => {
         
-//         if(dificultad === 'Facíl'){
+        if(dificultad === 'Facíl'){
 
-//             inicio.remove();
+            inicio.remove();
 
-//             reload = {array: arrayImagesFacil, numCartas: 8};
+            reload = {array: arrayImagesFacil, numCartas: 8};
 
-//             container.style.gridTemplateColumns = 'repeat(4, 1fr)';
+            container.style.gridTemplateColumns = 'repeat(4, 1fr)';
 
-//             setTimeout(() => {
-//                 body.append(container);
-//                 SeleccionarCartasRandom(arrayImagesFacil, 8);
-//             }, 100);
-//         }
-//         else if(dificultad === 'Intermedio') {
+            setTimeout(() => {
+                body.append(container);
+                SeleccionarCartasRandom(arrayImagesFacil, 8);
+            }, 100);
+        }
+        else if(dificultad === 'Intermedio') {
 
-//             inicio.remove();
+            inicio.remove();
 
-//             reload = {array: arrayImagesNormal, numCartas: 12};
+            reload = {array: arrayImagesNormal, numCartas: 12};
 
-//             if(screen.width > 700) container.style.gridTemplateColumns = 'repeat(6, 1fr)';
-//             else container.style.gridTemplateColumns = 'repeat(4, 1fr)';
+            if(screen.width > 700) container.style.gridTemplateColumns = 'repeat(6, 1fr)';
+            else container.style.gridTemplateColumns = 'repeat(4, 1fr)';
 
-//             setTimeout(() => {
-//                 body.append(container);
-//                 SeleccionarCartasRandom(arrayImagesNormal, 12);
-//             }, 100)
-//         }
-//         else if(dificultad === '1 vs 1') {
+            setTimeout(() => {
+                body.append(container);
+                SeleccionarCartasRandom(arrayImagesNormal, 12);
+            }, 100)
+        }
+        else if(dificultad === '1 vs 1') {
 
-//             crearVentanaEmergente();
+            crearVentanaEmergente();
 
-//             // setTimeout(() => {
+            // setTimeout(() => {
 
-//             //     alert('Modo de juego 1 vs 1 está en producción');
-//             // }, 1000)
-//         }
+            //     alert('Modo de juego 1 vs 1 está en producción');
+            // }, 1000)
+        }
 
-//     });
-// }
+    });
+}
